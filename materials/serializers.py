fc983f76165a792057bf43b69d00d9dca42144de
даша рамзер <kda2000.123@yandex.ru>
2026-09-03 21:16:13 +0300

    исправили код

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,11 +3,14 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from materials.models import Course, Lesson
 
 
-class CourseSerializer(ModelSerializer):
-    lessons = SerializerMethodField()
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
-    def get_lessons(self, course):
-        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
+
+class CourseSerializer(ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
 
     lessons_count = SerializerMethodField()
 
@@ -17,10 +20,4 @@ class CourseSerializer(ModelSerializer):
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = "__all__"
